9778ba461ccab87636ecaa1082863297ecf60193
Jonathan Guyer <guyer@nist.gov>
2017-11-09 22:17:59 -0500

    Remove pressure outlet gradient

--- problem5.py
+++ problem5.py
@@ -90,9 +90,9 @@ xVelocity.faceGrad.constrain([[0.], [0.]], outlet)
 yVelocity.constrain(0., walls | inlet | outlet)
 
 # pressure.constrain(0., top_right)
-pressure.faceGrad.constrain([[density * gravity[0]], [0.]], outlet)
+# pressure.faceGrad.constrain([[density * gravity[0]], [0.]], outlet)
 pressureCorrection.constrain(0., top_right)
-pressureCorrection.faceGrad.constrain([[0.], [0.]], outlet)
+# pressureCorrection.faceGrad.constrain([[0.], [0.]], outlet)
 
 # pressureCorrection.constrain(0., outlet)
 
